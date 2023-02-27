d683daff927293c479fe5a81619a57ed233b578d
ham.komcham <komchan.ham@gmail.com>
2023-02-27 10:46:07 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program {
+    static void Main(string[] args){
+        int count = 0;
+        int i = 0;
+
+        while(1 < 10) {
+            int x = int.Parse(Console.ReadLine());
+            i++;
+            if (x % 5 == 0) {
+                continue;
+            }
+        Console.WriteLine("even");
+        count++;
+        }
+    Console.WriteLine("Total: {0}", count);
+    }
+}
